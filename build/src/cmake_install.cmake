1acd3ce0443487c9de3eb579b3d0de42f4a4edb7
Yoana <yoana.zhelyazkova@bg.bosch.com>
2024-05-22 10:30:18 +0300

    OS removed

--- build/src/cmake_install.cmake
+++ build/src/cmake_install.cmake
@@ -59,11 +59,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/diplomna/TPMS/build/src/OS/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
   include("C:/diplomna/TPMS/build/src/Timer/cmake_install.cmake")
 endif()
 
